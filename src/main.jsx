bc4757fee1dddeb29b7843c32195ab93fefbf4fe
tymur-lvov <155083435+tymur-lvov@users.noreply.github.com>
2024-05-15 11:14:52 +0300

    Redux-Persistor configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
+    <PersistGate loading={null} persistor={persistor}></PersistGate>
   </Provider>
 );
